b0dfb544d50d3f95e311724f32a66c69f9e1b9d9
Viktor Cherenkov <viktor@Viktors-MacBook-Air.local>
2025-05-28 21:08:39 -0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yourname/GoSandbox
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	b := 20
+	c := a + b
+	fmt.Println("Sum of a and b is:", c)
+	fmt.Println("This is a simple Go program.")
+}
